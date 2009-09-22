e45a4d635dd1044cbbcb67521799f735087f4c4d
Gwenole Beauchesne <gbeauchesne@splitted-desktop.com>
2009-09-22 13:03:23 +0000

    Don't search LIBGL_DRIVERS_PATH, stick to LIBVA_DRIVERS_PATH only or the default path.

--- va/va.c
+++ va/va.c
@@ -135,10 +135,6 @@ static VAStatus va_openDriver(VADisplay dpy, char *driver_name)
     {
         /* don't allow setuid apps to use LIBVA_DRIVERS_PATH */
         search_path = getenv("LIBVA_DRIVERS_PATH");
-        if (!search_path)
-        {
-            search_path = getenv("LIBGL_DRIVERS_PATH");
-        }
     }
     if (!search_path)
     {
