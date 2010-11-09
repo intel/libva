24701e5083649539d6a57f8d8aa0ec916eaf27c8
Xiang, Haihao <haihao.xiang@intel.com>
2010-11-09 13:30:39 +0800

    i965_drv_video: fix surface dimension
    It is needed for Y-tiled surface
    
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>

--- i965_drv_video/i965_drv_video.c
+++ i965_drv_video/i965_drv_video.c
@@ -417,8 +417,8 @@ i965_CreateSurfaces(VADriverContextP ctx,
         obj_surface->subpic = VA_INVALID_ID;
         obj_surface->orig_width = width;
         obj_surface->orig_height = height;
-        obj_surface->width = ALIGN(obj_surface->orig_width, 16);
-        obj_surface->height = ALIGN(obj_surface->orig_height, 16);
+        obj_surface->width = ALIGN(obj_surface->orig_width, 128);
+        obj_surface->height = ALIGN(obj_surface->orig_height, 32);
         obj_surface->size = SIZE_YUV420(obj_surface->width, obj_surface->height);
         obj_surface->flags = SURFACE_REFERENCED;
         obj_surface->bo = NULL;
