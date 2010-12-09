78d9fb9c0d95dc5abd2272d60009cd88ffae33e7
Fei Jiang <fei.jiang@intel.com>
2010-12-09 16:32:18 +0800

    Compile vainfo in Android.

--- Android.mk
+++ Android.mk
@@ -1,7 +1,4 @@
 # Recursive call sub-folder Android.mk
 #
-# include $(call all-subdir-makefiles)
-LOCAL_PATH := $(my-dir)
-
-include $(LOCAL_PATH)/va/Android.mk
+ include $(call all-subdir-makefiles)
 
