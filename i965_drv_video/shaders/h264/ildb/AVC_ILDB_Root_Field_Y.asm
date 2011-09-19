6f2ee2b95a69c6b9c86a95dad91b12a5d9586610
Xiang, Haihao <haihao.xiang@intel.com>
2011-09-19 10:02:39 +0800

    va: remove i965 driver from libva repository
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>

--- i965_drv_video/shaders/h264/ildb/AVC_ILDB_Root_Field_Y.asm
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright © <2010>, Intel Corporation.
- *
- * This program is licensed under the terms and conditions of the
- * Eclipse Public License (EPL), version 1.0.  The full text of the EPL is at
- * http://www.opensource.org/licenses/eclipse-1.0.php.
- *
- */
-#include "AVC_ILDB_Root_Y.asm"
