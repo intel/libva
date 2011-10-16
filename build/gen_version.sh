5b82650dcc150c71ff70d144d1f0fe95afeb291f
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2011-10-16 12:08:52 +0200

    API: fix versioning.
    We have to consider three distinct versions at this time: the VA-API
    version, the library package version number, and the DSO version.
    
    * VA-API version:
    - increment major for any ABI change (which shall not occur!)
    - increment minor for any interface change (e.g. new or modified function)
    - increment micro for any other change (e.g. new flag, new codec definitions)
    - reset micro version to zero when minor version is incremented
    - reset minor version to zero when major version is incremented
    
    * libva package version number:
    - major version is automatically generated from VA-API major version
    - minor version is automatically generated from VA-API minor version
    - increment micro for any library release
    - reset micro version to zero when VA-API major or minor version is incremented
    
    * DSO version:
    The SONAME shall remain to libva.so.1 for VA-API 0.x.y as long as the ABI
    is not changed. Thus, the library name is generated as libva.<x>.<y>.0 where
    <x> = VA-API major version + 1
    <y> = 100 * VA-API minor version + VA-API micro version
    
    For example:
    VA-API 0.32.0 generates libva.so.1.3200.0 (libva 1.0.14 as of today)
    VA-API 0.34.1 generates libva.so.1.3401.0 (e.g. libva 1.2.1)
    VA-API 1.2.13 generates libva.so.2.213.0  (e.g. libva 2.2.13)
    
    Signed-off-by: Gwenole Beauchesne <gwenole.beauchesne@intel.com>

--- build/gen_version.sh
+++ build/gen_version.sh
@@ -12,21 +12,21 @@ parse_configure() {
 }
 
 if test -f "${libva_topdir}/configure.ac"; then
-    libva_major_version=`parse_configure_ac libva_major_version`
-    libva_minor_version=`parse_configure_ac libva_minor_version`
-    libva_micro_version=`parse_configure_ac libva_micro_version`
+    va_api_major_version=`parse_configure_ac va_api_major_version`
+    va_api_minor_version=`parse_configure_ac va_api_minor_version`
+    va_api_micro_version=`parse_configure_ac va_api_micro_version`
 elif test -f "${libva_topdir}/configure"; then
-    libva_major_version=`parse_configure LIBVA_MAJOR_VERSION`
-    libva_minor_version=`parse_configure LIBVA_MINOR_VERSION`
-    libva_micro_version=`parse_configure LIBVA_MICRO_VERSION`
+    va_api_major_version=`parse_configure VA_API_MAJOR_VERSION`
+    va_api_minor_version=`parse_configure VA_API_MINOR_VERSION`
+    va_api_micro_version=`parse_configure VA_API_MICRO_VERSION`
 else
     echo "ERROR: configure or configure.ac file not found in $libva_topdir/"
     exit 1
 fi
-libva_version="$libva_major_version.$libva_minor_version.$libva_micro_version"
+va_api_version="$va_api_major_version.$va_api_minor_version.$va_api_micro_version"
 
-sed -e "s/@LIBVA_MAJOR_VERSION@/${libva_major_version}/" \
-    -e "s/@LIBVA_MINOR_VERSION@/${libva_minor_version}/" \
-    -e "s/@LIBVA_MICRO_VERSION@/${libva_micro_version}/" \
-    -e "s/@LIBVA_VERSION@/${libva_version}/" \
+sed -e "s/@VA_API_MAJOR_VERSION@/${va_api_major_version}/" \
+    -e "s/@VA_API_MINOR_VERSION@/${va_api_minor_version}/" \
+    -e "s/@VA_API_MICRO_VERSION@/${va_api_micro_version}/" \
+    -e "s/@VA_API_VERSION@/${va_api_version}/" \
     $version_h_in
