41fd38f63b270ef4c4e29d1c0889ffd8ba65a2de
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2012-07-02 13:44:40 +0200

    drm: ensure DRM connection is authenticated.
    Make sure the DRM connection is authenticated prior to returning a
    valid display from vaGetDisplayDRM(). If an X or Wayland server is
    running then authentication will also fail.
    
    Signed-off-by: Gwenole Beauchesne <gwenole.beauchesne@intel.com>

---
+++ va/drm/va_drm_auth.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2012 Intel Corporation. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sub license, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+ * IN NO EVENT SHALL PRECISION INSIGHT AND/OR ITS SUPPLIERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "sysdeps.h"
+#include <unistd.h>
+#include <xf86drm.h>
+#include "va_drm_auth.h"
+
+/* Checks whether DRM connection is authenticated */
+bool
+va_drm_is_authenticated(int fd)
+{
+    pid_t client_pid;
+    int i, auth, pid, uid;
+    unsigned long magic, iocs;
+    bool is_authenticated = false;
+
+    client_pid = getpid();
+    for (i = 0; !is_authenticated; i++) {
+        if (drmGetClient(fd, i, &auth, &pid, &uid, &magic, &iocs) != 0)
+            break;
+        is_authenticated = auth && pid == client_pid;
+    }
+    return is_authenticated;
+}
+
+/* Try to authenticate the DRM connection with the supplied magic id */
+bool
+va_drm_authenticate(int fd, uint32_t magic)
+{
+    /* XXX: try to authenticate through Wayland, X11, etc. */
+    /* Default: root + master privs are needed for the following call */
+    return drmAuthMagic(fd, magic) == 0;
+}
