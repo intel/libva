98ffec1f93df5d3b624879852619f72310e3a3a2
Haitao Huang <haitao.huang@intel.com>
2012-07-06 13:52:18 +0200

    test: remove trailing whitespaces in Android.mk files.
    Signed-off-by: Haitao Huang <haitao.huang@intel.com>

--- test/basic/Android.mk
+++ test/basic/Android.mk
@@ -9,14 +9,14 @@ LOCAL_SRC_FILES := \
   test_01.c	\
 
 LOCAL_CFLAGS += \
-    -DANDROID  
+    -DANDROID
 
 LOCAL_C_INCLUDES += \
   $(TARGET_OUT_HEADERS)/libva	\
   $(TOPDIR)/hardware/intel/libva/va/
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE :=	test_001 
+LOCAL_MODULE :=	test_001
 
 LOCAL_SHARED_LIBRARIES := libva-android libva libdl libdrm libcutils libutils libui libsurfaceflinger_client
 
@@ -30,14 +30,14 @@ LOCAL_SRC_FILES := \
   test_02.c	\
 
 LOCAL_CFLAGS += \
-    -DANDROID  
+    -DANDROID
 
 LOCAL_C_INCLUDES += \
   $(TARGET_OUT_HEADERS)/libva	\
   $(TOPDIR)/hardware/intel/libva/va/
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE :=	test_02_android 
+LOCAL_MODULE :=	test_02_android
 
 LOCAL_SHARED_LIBRARIES := libva-android libva libdl libdrm libcutils libutils libui libsurfaceflinger_client
 
@@ -51,14 +51,14 @@ LOCAL_SRC_FILES := \
   test_03.c	\
 
 LOCAL_CFLAGS += \
-    -DANDROID  
+    -DANDROID
 
 LOCAL_C_INCLUDES += \
   $(TARGET_OUT_HEADERS)/libva	\
   $(TOPDIR)/hardware/intel/libva/va/
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE :=	test_03_android 
+LOCAL_MODULE :=	test_03_android
 
 LOCAL_SHARED_LIBRARIES := libva-android libva libdl libdrm libcutils libutils libui libsurfaceflinger_client
 
@@ -72,14 +72,14 @@ LOCAL_SRC_FILES := \
   test_04.c	\
 
 LOCAL_CFLAGS += \
-    -DANDROID  
+    -DANDROID
 
 LOCAL_C_INCLUDES += \
   $(TARGET_OUT_HEADERS)/libva	\
   $(TOPDIR)/hardware/intel/libva/va/
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE :=	test_04_android 
+LOCAL_MODULE :=	test_04_android
 
 LOCAL_SHARED_LIBRARIES := libva-android libva libdl libdrm libcutils libutils libui libsurfaceflinger_client
 
@@ -93,14 +93,14 @@ LOCAL_SRC_FILES := \
   test_05.c	\
 
 LOCAL_CFLAGS += \
-    -DANDROID  
+    -DANDROID
 
 LOCAL_C_INCLUDES += \
   $(TARGET_OUT_HEADERS)/libva	\
   $(TOPDIR)/hardware/intel/libva/va/
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE :=	test_05_android 
+LOCAL_MODULE :=	test_05_android
 
 LOCAL_SHARED_LIBRARIES := libva-android libva libdl libdrm libcutils libutils libui libsurfaceflinger_client
 
@@ -114,14 +114,14 @@ LOCAL_SRC_FILES := \
   test_06.c	\
 
 LOCAL_CFLAGS += \
-    -DANDROID  
+    -DANDROID
 
 LOCAL_C_INCLUDES += \
   $(TARGET_OUT_HEADERS)/libva	\
   $(TOPDIR)/hardware/intel/libva/va/
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE :=	test_06_android 
+LOCAL_MODULE :=	test_06_android
 
 LOCAL_SHARED_LIBRARIES := libva-android libva libdl libdrm libcutils libutils libui libsurfaceflinger_client
 
@@ -135,14 +135,14 @@ LOCAL_SRC_FILES := \
   test_07.c	\
 
 LOCAL_CFLAGS += \
-    -DANDROID  
+    -DANDROID
 
 LOCAL_C_INCLUDES += \
   $(TARGET_OUT_HEADERS)/libva	\
   $(TOPDIR)/hardware/intel/libva/va/
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE :=	test_07_android 
+LOCAL_MODULE :=	test_07_android
 
 LOCAL_SHARED_LIBRARIES := libva-android libva libdl libdrm libcutils libutils libui libsurfaceflinger_client
 
@@ -156,14 +156,14 @@ LOCAL_SRC_FILES := \
   test_08.c	\
 
 LOCAL_CFLAGS += \
-    -DANDROID  
+    -DANDROID
 
 LOCAL_C_INCLUDES += \
   $(TARGET_OUT_HEADERS)/libva	\
   $(TOPDIR)/hardware/intel/libva/va/
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE :=	test_08_android 
+LOCAL_MODULE :=	test_08_android
 
 LOCAL_SHARED_LIBRARIES := libva-android libva libdl libdrm libcutils libutils libui libsurfaceflinger_client
 
@@ -177,14 +177,14 @@ LOCAL_SRC_FILES := \
   test_09.c	\
 
 LOCAL_CFLAGS += \
-    -DANDROID  
+    -DANDROID
 
 LOCAL_C_INCLUDES += \
   $(TARGET_OUT_HEADERS)/libva	\
   $(TOPDIR)/hardware/intel/libva/va/
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE :=	test_09_android 
+LOCAL_MODULE :=	test_09_android
 
 LOCAL_SHARED_LIBRARIES := libva-android libva libdl libdrm libcutils libutils libui libsurfaceflinger_client
 
@@ -198,14 +198,14 @@ LOCAL_SRC_FILES := \
   test_10.c	
 
 LOCAL_CFLAGS += \
-    -DANDROID  
+    -DANDROID
 
 LOCAL_C_INCLUDES += \
   $(TARGET_OUT_HEADERS)/libva	\
   $(TOPDIR)/hardware/intel/libva/va/
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE :=	test_10_android 
+LOCAL_MODULE :=	test_10_android
 
 LOCAL_SHARED_LIBRARIES := libva-android libva libdl libdrm libcutils libutils libui libsurfaceflinger_client
 
@@ -219,14 +219,14 @@ LOCAL_SRC_FILES := \
   test_11.c	
 
 LOCAL_CFLAGS += \
-    -DANDROID  
+    -DANDROID
 
 LOCAL_C_INCLUDES += \
   $(TARGET_OUT_HEADERS)/libva	\
   $(TOPDIR)/hardware/intel/libva/va/
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE :=	test_11_android 
+LOCAL_MODULE :=	test_11_android
 
 LOCAL_SHARED_LIBRARIES := libva-android libva libdl libdrm libcutils libutils libui libsurfaceflinger_client
 
