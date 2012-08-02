a0365565ba5186edeba0bcda8b65cb4288e62470
Daniel Charles <daniel.charles@intel.com>
2012-08-02 10:30:44 -0700

    android: use Android logging utilities.
    Log functions available are now Android compatible and log statements
    will show on user space android logging system.
    
    Defined a LOG_TAG and make it available for all versions of Android.
    
    Signed-off-by: Daniel Charles <daniel.charles@intel.com>
    Signed-off-by: Gwenole Beauchesne <gwenole.beauchesne@intel.com>

--- va/sysdeps.h
+++ va/sysdeps.h
@@ -40,6 +40,30 @@
 # define Bool  int
 # define True  1
 # define False 0
+
+/* Android logging utilities */
+# define LOG_TAG "lib-va"
+# include <utils/Log.h>
+
+# ifdef ANDROID_ALOG
+#  define va_log_error(buffer)  do { ALOGE("%s", buffer); } while (0)
+#  define va_log_info(buffer)   do { ALOGI("%s", buffer); } while (0)
+# elif ANDROID_LOG
+#  define va_log_error(buffer)  do { LOGE("%s", buffer); } while (0)
+#  define va_log_info(buffer)   do { LOGI("%s", buffer); } while (0)
+# endif
+#endif
+
+#ifndef va_log_error
+#define va_log_error(buffer) do {                       \
+        fprintf(stderr, "libva error: %s", buffer);     \
+    } while (0)
+#endif
+
+#ifndef va_log_info
+#define va_log_info(buffer) do {                        \
+        fprintf(stderr, "libva info: %s", buffer);      \
+    } while (0)
 #endif
 
 #if defined __GNUC__ && defined HAVE_GNUC_VISIBILITY_ATTRIBUTE
