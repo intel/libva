726898f577b9b7051f1c38f9c59a94f5e549b1bb
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2012-09-28 10:09:51 +0200

    tests: include "sysdeps.h" wherever necessary.
    Use "sysdeps.h" header instead of "config.h" so that to include the
    necessary definitions for Android.
    
    Signed-off-by: Gwenole Beauchesne <gwenole.beauchesne@intel.com>

--- test/common/va_display.c
+++ test/common/va_display.c
@@ -22,9 +22,7 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#ifdef HAVE_CONFIG_H
-# include "config.h"
-#endif
+#include "sysdeps.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
--- test/vainfo/vainfo.c
+++ test/vainfo/vainfo.c
@@ -22,6 +22,7 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include "sysdeps.h"
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
