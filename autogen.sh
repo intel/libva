b1f335439285b2b4c171ee46d241df3a5475a94a
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2012-09-28 09:53:20 +0200

    autogen: modernize configure script generator.
    Make it possible to run autogen.sh from a different location from the
    original source directory. Make execution of the `configure' script
    optional. i.e. don't run it by default if NO_CONFIGURE variable is set.
    
    Signed-off-by: Gwenole Beauchesne <gwenole.beauchesne@intel.com>

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Copyright (c) 2007 Intel Corporation. All Rights Reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
@@ -20,6 +21,31 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#! /bin/sh
-autoreconf -v --install
-./configure "$@"
+PROJECT="libva"
+
+test -n "$srcdir" || srcdir="`dirname \"$0\"`"
+test -n "$srcdir" || srcdir=.
+
+if ! test -f "$srcdir/configure.ac"; then
+    echo "Failed to find the top-level $PROJECT directory"
+    exit 1
+fi
+
+olddir="`pwd`"
+cd "$srcdir"
+
+mkdir -p m4
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+    echo "*** No autoreconf found ***"
+    exit 1
+else
+    autoreconf -v --install || exit $?
+fi
+
+cd "$olddir"
+
+if test -z "$NO_CONFIGURE"; then
+    $srcdir/configure "$@" && echo "Now type 'make' to compile $PROJECT."
+fi
