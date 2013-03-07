523df13207814f376bec03ced04571b97aae86f4
Austin Yuan <shengquan.yuan@gmail.com>
2013-03-07 10:52:11 +0800

    vaPutSurface test on Android: fix build isste on Android JB
    Change-Id: Ibfb3d6a653f9af924422edb052e751b84015ffd0
    Signed-off-by: Austin Yuan <shengquan.yuan@gmail.com>

--- test/decode/Android.mk
+++ test/decode/Android.mk
@@ -21,7 +21,7 @@ LOCAL_C_INCLUDES += \
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE :=	mpeg2vldemo
 
-LOCAL_SHARED_LIBRARIES := libva-android libva libdl libdrm libcutils libutils libgui
+LOCAL_SHARED_LIBRARIES := libva libva-android libdl libdrm libcutils libutils libgui
 
 include $(BUILD_EXECUTABLE)
 
