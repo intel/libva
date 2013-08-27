quanganh2627/hardware_intel_libva

Add support for dumping AVC short format related fields in libva
BZ: 126158

num_ref_idx_l0_default_active_minus1 and num_ref_idx_l1_default_active_minus1
are added in H264PictureParameterBuffer. Their values
should be printed out when dumping the libva buffers to
ease the debugging.

Change-Id: I09bedc1fa3163b8dfd5c9831ff92cf4abaa81772
Signed-off-by: wfeng6 <wei.feng@intel.com>
Reviewed-on: http://android.intel.com:8080/120684
Reviewed-by: Shi, PingX <pingx.shi@intel.com>
Tested-by: Shi, PingX <pingx.shi@intel.com>
Reviewed-by: buildbot <buildbot@intel.com>
Tested-by: buildbot <buildbot@intel.com>

va/va_trace.c
@@ -1312,6 +1312,9 @@ static void va_TraceVAPictureParameterBufferH264(
     va_TraceFlagIfNotZero(idx, "\t\tredundant_pic_cnt_present_flag", p->pic_fields.bits.redundant_pic_cnt_present_flag);
     va_TraceFlagIfNotZero(idx, "\t\treference_pic_flag", p->pic_fields.bits.reference_pic_flag);
     va_TraceMsg(idx, "\tframe_num = %d\n", p->frame_num);
+    va_TraceMsg(idx, "\tnum_ref_idx_l0_default_active_minus1 = %d\n", p->num_ref_idx_l0_default_active_minus1);
+    va_TraceMsg(idx, "\tnum_ref_idx_l1_default_active_minus1 = %d\n", p->num_ref_idx_l1_default_active_minus1);
+
     va_TraceMsg(idx, NULL);
 
     return;

intel/libva

Change-Id: I4f543f1c2d12e2cfb7899bb677442fd96d1ec280

va/va_trace.c
@@ -1300,6 +1300,9 @@ static void va_TraceVAPictureParameterBufferH264(
     va_TraceFlagIfNotZero(trace_ctx, "\t\tredundant_pic_cnt_present_flag", p->pic_fields.bits.redundant_pic_cnt_present_flag);
     va_TraceFlagIfNotZero(trace_ctx, "\t\treference_pic_flag", p->pic_fields.bits.reference_pic_flag);
     va_TraceMsg(trace_ctx, "\tframe_num = %d\n", p->frame_num);
+    va_TraceMsg(trace_ctx, "\tnum_ref_idx_l0_default_active_minus1 = %d\n", p->num_ref_idx_l0_default_active_minus1);
+    va_TraceMsg(trace_ctx, "\tnum_ref_idx_l1_default_active_minus1 = %d\n", p->num_ref_idx_l1_default_active_minus1);
+    
     va_TraceMsg(trace_ctx, NULL);
 
     return;