0d9bbfe49abc78086cfac2941d85897627490d46
Xiang, Haihao <haihao.xiang@intel.com>
2014-11-14 16:44:10 +0800

    libva 1.5.0.pre1 for development
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>
    (cherry picked from commit 2fb37255e7c9ccb76e65ea74466f5ffafe9c68c0)
    
    Conflicts:
    	configure.ac

--- va/va.c
+++ va/va.c
@@ -246,6 +246,7 @@ static VAStatus va_openDriver(VADisplay dpy, char *driver_name)
                 int minor;
             } compatible_versions[] = {
                 { VA_MAJOR_VERSION, VA_MINOR_VERSION },
+                { 0, 36 },
                 { 0, 35 },
                 { 0, 34 },
                 { 0, 33 },
