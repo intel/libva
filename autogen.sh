e012fef185dd1b4e3814a5555e2903c25e6f2a69
Sean V Kelley <seanvk@posteo.de>
2017-01-21 22:40:37 -0800

    test commit for CI
    Signed-off-by: Sean V Kelley <seanvk@posteo.de>

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2007 Intel Corporation. All Rights Reserved.
+# Copyright (c) 2017 Intel Corporation. All Rights Reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
