f2a248b114b8398d2982328aaac61a7d458c7796
Mark Thompson <sw@jkqxz.net>
2017-02-04 16:14:45 +0000

    drm: Add mapping of amdgpu kernel driver to radeonsi Mesa driver
    Fixes #24.
    
    Signed-off-by: Mark Thompson <sw@jkqxz.net>

--- va/drm/va_drm_utils.c
+++ va/drm/va_drm_utils.c
@@ -43,6 +43,7 @@ static const struct driver_name_map g_driver_name_map[] = {
     { "hybrid",     6, "hybrid" }, // Intel OTC Hybrid driver
     { "nouveau",    7, "nouveau"  }, // Mesa Gallium driver
     { "radeon",     6, "r600"     }, // Mesa Gallium driver
+    { "amdgpu",     6, "radeonsi" }, // Mesa Gallium driver
     { NULL, }
 };
 
