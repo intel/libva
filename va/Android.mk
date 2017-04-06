b4c6fb0196bb5de06da783722a53f2909ae7bae5
Chih-Wei Huang <cwhuang@linux.org.tw>
2017-04-06 13:18:37 +0800

    android: change libva drivers path
    Use the path similar to linux desktop.
    
    Signed-off-by: Chih-Wei Huang <cwhuang@linux.org.tw>
    (cherry picked from commit a42e50357d6b76a949641ed6c7f9557e2867be60)

--- va/Android.mk
+++ va/Android.mk
@@ -25,8 +25,8 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-LIBVA_DRIVERS_PATH_32 = /system/lib
-LIBVA_DRIVERS_PATH_64 = /system/lib64
+LIBVA_DRIVERS_PATH_32 := /system/lib/dri
+LIBVA_DRIVERS_PATH_64 := /system/lib64/dri
 
 include $(CLEAR_VARS)
 
