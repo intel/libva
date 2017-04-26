63e14fc10177f88fb1350681120b4bce15ea81a7
Chih-Wei Huang <cwhuang@linux.org.tw>
2017-04-26 13:32:29 +0800

    android: change VA_DRIVERS_PATH to /vendor
    Signed-off-by: Chih-Wei Huang <cwhuang@linux.org.tw>

--- va/Android.mk
+++ va/Android.mk
@@ -25,8 +25,8 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-LIBVA_DRIVERS_PATH_32 := /system/lib/dri
-LIBVA_DRIVERS_PATH_64 := /system/lib64/dri
+LIBVA_DRIVERS_PATH_32 := /vendor/lib/dri
+LIBVA_DRIVERS_PATH_64 := /vendor/lib64/dri
 
 include $(CLEAR_VARS)
 
