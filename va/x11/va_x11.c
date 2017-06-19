a7b7cf7ba20c2b5b41e052508c3970f473f54f92
Mark Thompson <sw@jkqxz.net>
2017-06-19 19:09:12 +0100

    Move some common macro definitions to the new internal header
    Signed-off-by: Mark Thompson <sw@jkqxz.net>

--- va/x11/va_x11.c
+++ va/x11/va_x11.c
@@ -195,8 +195,6 @@ VADisplay vaGetDisplay (
   return dpy;
 }
 
-#define CTX(dpy) (((VADisplayContextP)dpy)->pDriverContext)
-#define CHECK_DISPLAY(dpy) if( !vaDisplayIsValid(dpy) ) { return VA_STATUS_ERROR_INVALID_DISPLAY; }
 
 void va_TracePutSurface (
     VADisplay dpy,
