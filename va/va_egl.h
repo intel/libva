4086c98ec16834b4b8649f260a78f2c864e9779b
Xiang, Haihao <haihao.xiang@intel.com>
2017-06-19 16:25:39 +0800

    Delete libva-tpi and libva-egl backends
    The APIs/data structures defined in va_tpi.h and va_egl.h are used
    to implement buffer sharing between libva and the 3rd party libraries.
    We have an official buffer sharing machnism in VA core, so libva-tpi and
    libva-egl are unnecessary any more.
    
    However va_egl.h or va_tpi.h is included uselessly in a few places,
    e.g. https://github.com/01org/libyami/blob/apache/vaapi/vaapidisplay.h#L22
    So va_egl.h and va_tpi.h are kept with a deprecation warning message.
    
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>

--- va/va_egl.h
+++ va/va_egl.h
@@ -8,11 +8,11 @@
  * distribute, sub license, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice (including the
  * next paragraph) shall be included in all copies or substantial portions
  * of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
@@ -21,49 +21,9 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef _VA_EGL_H_
+#define _VA_EGL_H_
 
-/*
- * Video Decode Acceleration -Backend API
- */
+#warning The APIs / data structures included in this file are deprecated
 
-#ifndef _VA_BACKEND_TPI_H_
-#define _VA_BACKEND_TPI_H_
-
-#include <va/va.h>
-#include <va/va_backend.h>
-#include <va/va_tpi.h>
-
-struct VADriverVTableTPI
-{
-    VAStatus (*vaCreateSurfacesWithAttribute) (
-        VADisplay dpy,
-        int width,
-        int height,
-        int format,
-        int num_surfaces,
-        VASurfaceID *surfaces,       /* out */
-        VASurfaceAttributeTPI *attribute_tpi
-        );
-    
-
-    VAStatus (*vaPutSurfaceBuf) (
-        VADriverContextP ctx,
-        VASurfaceID surface,
-        unsigned char* data,
-        int* data_len,
-        short srcx,
-        short srcy,
-        unsigned short srcw,
-        unsigned short srch,
-        short destx,
-        short desty,
-        unsigned short destw,
-        unsigned short desth,
-        VARectangle *cliprects, /* client supplied clip list */
-        unsigned int number_cliprects, /* number of clip rects in the clip list */
-        unsigned int flags /* de-interlacing flags */
-        );
-};
-
-
-#endif /* _VA_BACKEND_TPI_H_ */
+#endif /* _VA_EGL_H_ */
