151cdeda44cefdae1c83192dc0321a38c21c8019
Xiang, Haihao <haihao.xiang@intel.com>
2018-02-02 08:49:37 +0800

    libva 2.1.1.pre1 for development
    Change-Id: I4baaa16602e7f22b0784f67859a861b91c2f0514
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>
    Signed-off-by: Romli, Khairul Anuar <khairul.anuar.romli@intel.com>
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-59281
    Reviewed-on: https://android.intel.com:443/621494

--- va/va.c
+++ va/va.c
@@ -402,7 +402,7 @@ static VAStatus va_openDriver(VADisplay dpy, char *driver_name)
                 int minor;
             } compatible_versions[] = {
                 { VA_MAJOR_VERSION, VA_MINOR_VERSION },
-				{ 0, 32},
+                { VA_MAJOR_VERSION, 0 },
                 { -1, -1}
             };
 
