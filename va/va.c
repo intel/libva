eaf819cebc5fb98db817d98b679ec2f1b76e7026
Jan Beich <jbeich@FreeBSD.org>
2020-01-15 19:08:17 +0000

    va/va: don't try to use RTLD_NODELETE on non-Android if not available
    ../va/va.c:455:64: error: use of undeclared identifier 'RTLD_NODELETE'
            handle = dlopen( driver_path, RTLD_NOW | RTLD_GLOBAL | RTLD_NODELETE );
                                                                   ^

--- va/va.c
+++ va/va.c
@@ -451,7 +451,7 @@ static VAStatus va_openDriver(VADisplay dpy, char *driver_name)
         }
 
         va_infoMessage(dpy, "Trying to open %s\n", driver_path);
-#ifndef ANDROID
+#if defined(RTLD_NODELETE) && !defined(ANDROID)
         handle = dlopen( driver_path, RTLD_NOW | RTLD_GLOBAL | RTLD_NODELETE );
 #else
         handle = dlopen( driver_path, RTLD_NOW| RTLD_GLOBAL);
