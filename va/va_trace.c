45afd79ae4b887ad06cfca7316ada5c2688513f9
Carl Zhang <carl.zhang@intel.com>
2020-02-24 10:56:22 -0500

    va:add new interface vaMapBuffer2 for map operation optimization
    add new vaMapBuffer2
    backend driver need some usage hint to optimize the operation
    
    Signed-off-by: Carl Zhang <carl.zhang@intel.com>

--- va/va_trace.c
+++ va/va_trace.c
@@ -1740,7 +1740,8 @@ static void va_TraceCodedBufferIVFHeader(struct trace_context *trace_ctx, void *
 void va_TraceMapBuffer(
     VADisplay dpy,
     VABufferID buf_id,    /* in */
-    void **pbuf           /* out */
+    void **pbuf,          /* out */
+    uint32_t flags       /* in */
 )
 {
     VABufferType type;
@@ -1761,6 +1762,7 @@ void va_TraceMapBuffer(
     TRACE_FUNCNAME(idx);
     va_TraceMsg(trace_ctx, "\tbuf_id=0x%x\n", buf_id);
     va_TraceMsg(trace_ctx, "\tbuf_type=%s\n", vaBufferTypeStr(type));
+    va_TraceMsg(trace_ctx, "\tflags = 0x%x\n", flags);
     if ((pbuf == NULL) || (*pbuf == NULL))
         return;
 
