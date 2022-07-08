317c0fbea5405a27bf58505a6bc6b1f6414beb71
Emil Velikov <emil.velikov@collabora.com>
2022-07-08 14:15:59 +0100

    drm: fallback to drm driver name == va driver name
    Currently we have a bunch of dummy entries, where both driver names
    match. In addition, there is at least one more PR open to add a similar
    mapping.
    
    In the DRI2 mapping, we already fallback to the original (DRI2 driver)
    name, so do the same here.
    
    Add a convenience va_DRM_GetDrmDriverName() helper while we're here.
    
    v2: Actually return the drm name
    
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

--- va/drm/va_drm_utils.c
+++ va/drm/va_drm_utils.c
@@ -48,27 +48,54 @@ static const struct driver_name_map g_driver_name_map[] = {
     { NULL,         NULL }
 };
 
+static char *
+va_DRM_GetDrmDriverName(int fd)
+{
+    drmVersionPtr drm_version = drmGetVersion(fd);
+    char *driver_name;
+
+    if (!drm_version)
+        return NULL;
+
+    driver_name = strdup(drm_version->name);
+    drmFreeVersion(drm_version);
+
+    return driver_name;
+}
+
 /* Returns the VA driver candidate num for the active display*/
 VAStatus
 VA_DRM_GetNumCandidates(VADriverContextP ctx, int * num_candidates)
 {
     struct drm_state * const drm_state = ctx->drm_state;
-    drmVersionPtr drm_version;
     int count = 0;
     const struct driver_name_map *m = NULL;
+    char *driver_name;
+
     if (!drm_state || drm_state->fd < 0)
         return VA_STATUS_ERROR_INVALID_DISPLAY;
-    drm_version = drmGetVersion(drm_state->fd);
-    if (!drm_version)
+
+    driver_name = va_DRM_GetDrmDriverName(drm_state->fd);
+    if (!driver_name)
         return VA_STATUS_ERROR_UNKNOWN;
+
     for (m = g_driver_name_map; m->key != NULL; m++) {
-        if (strcmp(m->key, drm_version->name) == 0) {
+        if (strcmp(m->key, driver_name) == 0) {
             count ++;
         }
     }
-    drmFreeVersion(drm_version);
+
+    free(driver_name);
+
+    /*
+     * If the drm driver name does not have a mapped vaapi driver name, then
+     * assume they have the same name.
+     */
+    if (count == 0)
+        count = 1;
+
     *num_candidates = count;
-    return count ? VA_STATUS_SUCCESS : VA_STATUS_ERROR_UNKNOWN;
+    return VA_STATUS_SUCCESS;
 }
 
 /* Returns the VA driver name for the active display */
@@ -76,8 +103,6 @@ VAStatus
 VA_DRM_GetDriverName(VADriverContextP ctx, char **driver_name_ptr, int candidate_index)
 {
     struct drm_state * const drm_state = ctx->drm_state;
-    drmVersionPtr drm_version;
-    char *driver_name = NULL;
     const struct driver_name_map *m;
     int current_index = 0;
 
@@ -86,28 +111,32 @@ VA_DRM_GetDriverName(VADriverContextP ctx, char **driver_name_ptr, int candidate
     if (!drm_state || drm_state->fd < 0)
         return VA_STATUS_ERROR_INVALID_DISPLAY;
 
-    drm_version = drmGetVersion(drm_state->fd);
-    if (!drm_version)
+    *driver_name_ptr = va_DRM_GetDrmDriverName(drm_state->fd);
+    if (!*driver_name_ptr)
         return VA_STATUS_ERROR_UNKNOWN;
 
     for (m = g_driver_name_map; m->key != NULL; m++) {
-        if (strcmp(m->key, drm_version->name) == 0) {
+        if (strcmp(m->key, *driver_name_ptr) == 0) {
             if (current_index == candidate_index) {
                 break;
             }
             current_index ++;
         }
     }
-    drmFreeVersion(drm_version);
 
+    /*
+     * If the drm driver name does not have a mapped vaapi driver name, then
+     * assume they have the same name.
+     */
     if (!m->name)
-        return VA_STATUS_ERROR_UNKNOWN;
+        return VA_STATUS_SUCCESS;
 
-    driver_name = strdup(m->name);
-    if (!driver_name)
+    /* Use the mapped vaapi driver name */
+    free(*driver_name_ptr);
+    *driver_name_ptr = strdup(m->name);
+    if (!*driver_name_ptr)
         return VA_STATUS_ERROR_ALLOCATION_FAILED;
 
-    *driver_name_ptr = driver_name;
     return VA_STATUS_SUCCESS;
 }
 
